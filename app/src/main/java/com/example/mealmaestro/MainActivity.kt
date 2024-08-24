6ab106e0b779a5e9d897ea21b97a5ea8b53dc4b9
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-08-24 19:45:01 +1200

    Added Requirements for Navigation menu

--- app/src/main/java/com/example/mealmaestro/MainActivity.kt
+++ app/src/main/java/com/example/mealmaestro/MainActivity.kt
@@ -6,18 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mealmaestro.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -27,13 +33,21 @@ class MainActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        // Set up the BottomNavigationView with NavController
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
+        bottomNavigationView.setupWithNavController(navController)
+
         binding.logOut.setOnClickListener {
             logOut()
         }
     }
 
-    private fun logOut(){
+    private fun logOut() {
         auth.signOut()
-        startActivity(Intent(this@MainActivity,LoginActivity::class.java))
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
     }
 }
