74b1bf3ac9f5d560273b4c15017eb308153e119f
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-08-24 21:08:16 +1200

    top navigation workingbut has bugs

--- app/src/main/java/com/example/mealmaestro/MainActivity.kt
+++ app/src/main/java/com/example/mealmaestro/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.mealmaestro
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.mealmaestro.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var auth: FirebaseAuth
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +27,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        auth = FirebaseAuth.getInstance()
+        drawerLayout = binding.drawerLayout
+
+        // Set up Toolbar
+        val toolbar: Toolbar = binding.topToolbar
+        setSupportActionBar(toolbar)
+
+        // Set up the ActionBarDrawerToggle for the navigation drawer
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
         // Set up the BottomNavigationView with NavController
         val navHostFragment = supportFragmentManager
@@ -40,14 +49,13 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.setupWithNavController(navController)
-
-        binding.logOut.setOnClickListener {
-            logOut()
-        }
     }
 
-    private fun logOut() {
-        auth.signOut()
-        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
