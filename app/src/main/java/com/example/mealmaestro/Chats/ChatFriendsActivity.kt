7c03105cc69e6ef3b95f3406a88543448bfaf819
Christian Hidalgo <145306497+Chidalgo007@users.noreply.github.com>
2024-08-26 16:43:21 +1200

    test

--- app/src/main/java/com/example/mealmaestro/Chats/ChatFriendsActivity.kt
+++ app/src/main/java/com/example/mealmaestro/Chats/ChatFriendsActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.mealmaestro.DataBase
+import com.example.mealmaestro.Helper.DataBase
 import com.example.mealmaestro.databinding.ActivityChatFriendsBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
