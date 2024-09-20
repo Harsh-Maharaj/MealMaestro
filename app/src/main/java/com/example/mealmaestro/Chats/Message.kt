5f79819158247a241af6feea6b6207a6f9ee1043
Harsh Maharaj <HarshMaharaj969@gmail.com>
2024-09-20 22:57:22 +1200

    Minor Bug Fix

--- app/src/main/java/com/example/mealmaestro/Chats/Message.kt
+++ app/src/main/java/com/example/mealmaestro/Chats/Message.kt
@@ -5,5 +5,8 @@ data class Message(
     val message: String = "", // The actual message content, defaults to an empty string
     val sender: String = "", // The UID (User ID) of the message sender, defaults to an empty string
     val receiverUid: String? = null, // The UID (User ID) of the message receiver, nullable, defaults to null
-    val timestamp: Long = System.currentTimeMillis() // The time when the message was created, defaults to the current system time
+    val timestamp: Long = System.currentTimeMillis(), // The time when the message was created, defaults to the current system time
+    val seen: Boolean = false, // Whether the message has been seen by the receiver
+    val delivered: Boolean = false, // Whether the message has been delivered to the receiver's device
+    var messageId: String? = null // Unique message ID, nullable
 )
